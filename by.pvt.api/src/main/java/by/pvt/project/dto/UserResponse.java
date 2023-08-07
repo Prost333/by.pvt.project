da296d9966e9c80097159a552efa5a202687ae53
prost333 <dimabolshoy333@yandex.by>
2023-08-07 10:21:22 +0300

    на проверку

--- by.pvt.api/src/main/java/by/pvt/project/dto/UserResponse.java
+++ by.pvt.api/src/main/java/by/pvt/project/dto/UserResponse.java
@@ -1,52 +1,5 @@
 package by.pvt.project.dto;
 
 public class UserResponse {
-    private int id;
-    private  String fullname;
-    private String login;
 
-
-    public UserResponse(int id, String fullname, String login) {
-        this.id = id;
-        this.fullname = fullname;
-        this.login = login;
-
-    }
-
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-
-    public String getFullname() {
-        return fullname;
-    }
-
-    public void setFullname(String fullname) {
-        this.fullname = fullname;
-    }
-
-    public String getLogin() {
-        return login;
-    }
-
-    public void setLogin(String login) {
-        this.login = login;
-    }
-
-
-
-    @Override
-    public String toString() {
-        return "UserResponse{" +
-                "id=" + id +
-                ", fullname='" + fullname + '\'' +
-                ", login='" + login + '\'' +
-                '}';
-    }
 }
--- by.pvt.core/src/main/java/by/pvt/project/repository/FileWorker.java
+++ by.pvt.core/src/main/java/by/pvt/project/repository/FileWorker.java
@@ -3,25 +3,7 @@ package by.pvt.project.repository;
 import java.io.*;
 
 public class FileWorker {
-    public static void serializeObject(Object object, String file) {
-        try (ObjectOutputStream objectOutputStream = new ObjectOutputStream(new FileOutputStream(file))) {
-            objectOutputStream.writeObject(object);
-        } catch (FileNotFoundException e) {
-//            e.printStackTrace();
-        } catch (IOException e) {
-//            e.printStackTrace();
-        }
-    }
 
-    public static Object deserializeObject(String file) {
-        try { ObjectInputStream objectInputStream = new ObjectInputStream(new FileInputStream(file));
-            Object list = objectInputStream.readObject();
-            objectInputStream.close();
-            return list;
-        } catch (Exception e) {
-//            e.printStackTrace();
-            return new Object();
-        }
-    }
+
 }
 
